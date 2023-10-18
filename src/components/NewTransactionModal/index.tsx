f7f25e76dfc22d2ecc8bbea38b04b9d1e601f683
Jairotsb <jairo_tunisse@outlook.com>
2023-10-18 18:38:17 -0300

    feat: search transaction added :godmode:

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -17,7 +17,7 @@ const newTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
-  // type: z.enum(['income', 'outcome']),
+  type: z.enum(['income', 'outcome']),
 })
 
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
@@ -72,17 +72,26 @@ export function NewTransactionModal() {
               {...register('category')}
             />
 
-            <TransactionType>
-              <TransactionTypeButton variant="income" value="income">
-                <ArrowCircleUp size={24} /> Entrada
-              </TransactionTypeButton>
+            <Controller
+              control={control}
+              name="type"
+              render={({ field }) => {
+                return (
+                  <TransactionType
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  >
+                    <TransactionTypeButton variant="income" value="income">
+                      <ArrowCircleUp size={24} /> Entrada
+                    </TransactionTypeButton>
 
-              <TransactionTypeButton variant="outcome" value="outcome">
-                <ArrowCircleDown size={24} /> Saída
-              </TransactionTypeButton>
-            </TransactionType>
-
-            <Controller control={control} />
+                    <TransactionTypeButton variant="outcome" value="outcome">
+                      <ArrowCircleDown size={24} /> Saída
+                    </TransactionTypeButton>
+                  </TransactionType>
+                )
+              }}
+            />
 
             <button type="submit" disabled={isSubmitting}>
               Cadastrar
